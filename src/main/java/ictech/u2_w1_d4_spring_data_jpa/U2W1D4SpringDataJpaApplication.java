a1e10f0ae202e16250e73d848c57f77cefc7ef4a
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-07-17 10:07:29 +0100

    Initial commit

---
+++ src/main/java/ictech/u2_w1_d4_spring_data_jpa/U2W1D4SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package ictech.u2_w1_d4_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D4SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D4SpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ictech/u2_w1_d4_spring_data_jpa/U2W1D4SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ictech.u2_w1_d4_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D4SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
